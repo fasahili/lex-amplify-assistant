884427a2f7eb30298196247413fbd6bdc478ab5f
Faris Al-Sahili <farisasahili@gmail.com>
2025-05-12 16:49:38 +0300

    integrate AWS Cognito with Amplify and implement full signup/login flow

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "frontend4b0f4b91": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
